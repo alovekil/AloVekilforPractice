bd461e89dacabbd8d4910a011fb0d30484603f42
alovekil <alovekil176@gmail.com>
2022-08-06 18:51:01 +0400

    123

---
+++ app/src/main/java/com/karimmammadov/alovekilforpractice/CustomerRegstrActivity.kt
@@ -0,0 +1,11 @@
+package com.karimmammadov.alovekilforpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerRegstrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_regstr)
+    }
+}
---
+++ app/src/main/java/com/karimmammadov/alovekilforpractice/EnterNumberActivity.kt
@@ -0,0 +1,11 @@
+package com.karimmammadov.alovekilforpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_number)
+    }
+}
