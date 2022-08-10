c2cc01c760d1181d978c9e2ecbc9f5578253b0c8
alovekil <alovekil176@gmail.com>
2022-08-10 12:41:24 +0400

    BIGcHANGE

--- app/src/main/java/com/karimmammadov/alovekilforpractice/LawyerRegisterActivity.kt
+++ app/src/main/java/com/karimmammadov/alovekilforpractice/LawyerRegisterActivity.kt
@@ -8,22 +8,10 @@ import kotlinx.android.synthetic.main.activity_lawyer_register.*
 import me.relex.circleindicator.CircleIndicator3
 
 class LawyerRegisterActivity : AppCompatActivity() {
-    private var layoutsList = mutableListOf<View>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lawyer_register)
 
-        view_pager2.adapter = ViewPageAdapter(layoutsList)
-        view_pager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-
-        val indicator = findViewById<CircleIndicator3>(R.id.indicator)
-        indicator.setViewPager(view_pager2)
-    }
-
-    private fun addToList(layout: View){
-        for(i in 1..2){
-            layoutsList.add(layout)
-        }
     }
 }
--- app/src/main/java/com/karimmammadov/alovekilforpractice/MainActivity.kt
+++ app/src/main/java/com/karimmammadov/alovekilforpractice/MainActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser!=null){
+            val intent = Intent(this,PinCodeActivity::class.java)
+        }
     }
 
     fun signUp(view:View){
