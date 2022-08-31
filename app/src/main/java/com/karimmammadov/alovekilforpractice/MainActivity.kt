64f8a4d27aab549fa3e594b622b2b6652b53f161
alovekil <alovekil176@gmail.com>
2022-09-01 02:58:45 +0400

    2;58

--- app/src/main/java/com/karimmammadov/alovekilforpractice/MainActivity.kt
+++ app/src/main/java/com/karimmammadov/alovekilforpractice/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.karimmammadov.alovekilforpractice.PinCode.CreatePasswordActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun signUp(view:View){
-            val intent = Intent(this@MainActivity, LawyerRegisterActivity::class.java)
+            val intent = Intent(this@MainActivity, ChooseSignUpActivity::class.java)
         startActivity(intent)
         finish()
         //big
